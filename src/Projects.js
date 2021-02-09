76f99577dd7289999860fe63ee7595d0b62ec9a4
Lisa Wand <lisavenneker21@gmail.com>
2021-02-08 19:16:18 -0500

    project cards trials

---
+++ src/Projects.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Projects
+
+// tot pop out cards:
+// https://codepen.io/akhil_001/pen/zoQdaO 
+
+// create hover card: 
+//https://codedaily.io/tutorials/62/Create-a-Hoverable-Display-Card-with-React-and-Emotion
