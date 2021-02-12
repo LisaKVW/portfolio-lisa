1149412f163fdaa8b9408023c7fdd042295c0848
Lisa Wand <lisavenneker21@gmail.com>
2021-02-12 14:59:45 -0500

    got good looking porfolio card and button show on hover to link site

--- src/Projects.js
+++ src/Projects.js
@@ -1,19 +1,23 @@
 import React from 'react'
+import './Projects.css'
 
 const Projects = () => {
   return (
     <div>
       <h2> Projects </h2>
-      <div class="row">
-        <div class="col s12 m6">
-          <div class="card">
-            <div class="card-image">
+      <div className="row">
+        <div className="col s12 m6" style={{ width: "30%" }}>
+          <div className="card" >
+            <div className="card-image">
               <img src="https://images.pexels.com/photos/708587/pexels-photo-708587.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
-              {/* <a class="btn-floating halfway-fab waves-effect waves-light btn right">Site</a>
-              <a class="btn-floating waves-effect waves-light btn right" style={{marginTop:"28px", marginRight: "23px"}}>Code</a> */}
+              {/* <a className="btn-floating halfway-fab waves-effect waves-light btn right">Site</a>
+              <a className="btn-floating waves-effect waves-light btn right" style={{marginTop:"28px", marginRight: "23px"}}>Code</a> */}
+              <p className="title"> The Dutch Chef </p>
+              <div className="overlay"></div>
+              <a href="https://the-dutch-chef.herokuapp.com/" > Site </a>
             </div>
-            <div class="card-content">
-              <p>The Dutch Chef </p>
+            <div className="card-content" style={{ padding: "8px" }} >
+              <p> Site to find delicious recipes and restaurant stories through the eyes of the Dutch Chef.</p>
             </div>
           </div>
         </div>
