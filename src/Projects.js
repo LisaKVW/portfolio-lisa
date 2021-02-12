b2472f09c6c55bb3d7aced707e793a06ddf4c691
Lisa Wand <lisavenneker21@gmail.com>
2021-02-11 23:21:15 -0500

    button testing project cards

--- src/Projects.js
+++ src/Projects.js
@@ -9,7 +9,8 @@ const Projects = () => {
           <div class="card">
             <div class="card-image">
               <img src="https://images.pexels.com/photos/708587/pexels-photo-708587.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
-              <a class="btn-floating halfway-fab waves-effect waves-light red darken-1" href="https://the-dutch-chef.herokuapp.com/"><i class="material-icons">language</i></a>
+              {/* <a class="btn-floating halfway-fab waves-effect waves-light btn right">Site</a>
+              <a class="btn-floating waves-effect waves-light btn right" style={{marginTop:"28px", marginRight: "23px"}}>Code</a> */}
             </div>
             <div class="card-content">
               <p>The Dutch Chef </p>
@@ -23,3 +24,5 @@ const Projects = () => {
 
 export default Projects
 
+// Source tip to have button show up upon hover:
+// https://codepen.io/philcheng/pen/YWyYwG 
