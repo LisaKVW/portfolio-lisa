73a47470554485a5d48a207da869a9140bb64da0
Lisa Wand <lisavenneker21@gmail.com>
2021-02-12 16:59:28 -0500

    5 cards for portfolio looking preety good

--- src/Projects.js
+++ src/Projects.js
@@ -17,12 +17,28 @@ const Projects = () => {
                 <a href="https://the-dutch-chef.herokuapp.com/" > Site </a>
               </div>
               <div className="card-content" style={{ padding: "8px", minHeight: "56px" }} >
-                <p> Site to find delicious recipes and restaurant stories through the eyes of the Dutch Chef.</p>
+                <p> This website is made with my own created recipe database using postgreSQL. As a user you can find delicious recipes and restaurant stories through the eyes of the Dutch Chef and sign-up for updates in the contact tab.</p>
               </div>
             </div>
           </div>
         </div>
         {/* CARD 2 */}
+        <div className="row">
+          <div className="col s12 m6" style={{ width: "auto" }}>
+            <div className="card" >
+              <div className="card-image">
+                <img src="https://images.pexels.com/photos/372959/pexels-photo-372959.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+                <p className="title"> Mixology at home </p>
+                <div className="overlay"></div>
+                <a href="https://mixology-at-home.netlify.app/" > Site </a>
+              </div>
+              <div className="card-content" style={{ padding: "8px", minHeight: "56px" }} >
+                <p> This app uses the cocktaildb API and allows you to find almost any cocktail by name. Had a long day, click the 'Chuck Norris' pun button!.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* CARD 3 */}
         <div className="row">
           <div className="col s12 m6" style={{ width: "auto" }}>
             <div className="card" >
@@ -33,11 +49,44 @@ const Projects = () => {
                 <a href="https://game-insider.herokuapp.com/" > Site </a>
               </div>
               <div className="card-content" style={{ padding: "8px", minHeight: "56px" }} >
-                <p> Site for all game lovers! Sign up/in and chat and read games.</p>
+                <p> This webiste uses the RAWG API for up-to-date video game information. Sign up/in and chat and read game news.</p>
               </div>
             </div>
           </div>
         </div>
+        {/* CARD 4 */}
+        <div className="row">
+          <div className="col s12 m6" style={{ width: "auto" }}>
+            <div className="card" >
+              <div className="card-image">
+                <img src="https://images.pexels.com/photos/1118873/pexels-photo-1118873.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+                <p className="title"> Weather React</p>
+                <div className="overlay"></div>
+                <a href="https://your-weather-react.netlify.app/" > Site </a>
+              </div>
+              <div className="card-content" style={{ padding: "8px", minHeight: "56px" }} >
+                <p> A simple weather app made with react. Use the app to find the weather of your searched location in Celcius and Fahrenheit. Weather data provided by OpenWeatherMap API.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* CARD 5 */}
+        <div className="row">
+          <div className="col s12 m6" style={{ width: "auto" }}>
+            <div className="card" >
+              <div className="card-image">
+                <img src="https://images.pexels.com/photos/219998/pexels-photo-219998.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+                <p className="title"> Weather Javascript</p>
+                <div className="overlay"></div>
+                <a href="https://your-weather-javas.netlify.app/" > Site </a>
+              </div>
+              <div className="card-content" style={{ padding: "8px", minHeight: "56px" }} >
+                <p> A simple app to find the weather of your searched location in Celcius and Fahrenheit, made with HTML, CSS and Javascript Weather data provided by OpenWeatherMap API.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* 2 bottom div's wrapper and div column gridTempl */}
       </div>
     </div>
   )
