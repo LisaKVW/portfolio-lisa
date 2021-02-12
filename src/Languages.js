8b0f5fd8d5965744fa4adea035ebf83425d45c8c
Lisa Wand <lisavenneker21@gmail.com>
2021-02-11 20:07:24 -0500

    working on cards for projects

--- src/Languages.js
+++ src/Languages.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Languages = () => {
   return (
     <div style={{ marginTop: "2%" }}>
+      <h1> Skills </h1>
       <img src="https://res.cloudinary.com/lisakvw/image/upload/v1612827956/portfolio-lisa/Languages2_xvijdl.png" />
     </div>
   )
--- src/Projects.js
+++ src/Projects.js
@@ -3,7 +3,20 @@ import React from 'react'
 const Projects = () => {
   return (
     <div>
-
+      <h2> Projects </h2>
+      <div class="row">
+        <div class="col s12 m6">
+          <div class="card">
+            <div class="card-image">
+              <img src="https://images.pexels.com/photos/708587/pexels-photo-708587.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+              <a class="btn-floating halfway-fab waves-effect waves-light red darken-1" href="https://the-dutch-chef.herokuapp.com/"><i class="material-icons">language</i></a>
+            </div>
+            <div class="card-content">
+              <p>The Dutch Chef </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
