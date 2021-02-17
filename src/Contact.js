7b3771fae93f1f63b519b718407646357da9b008
Lisa Wand <lisavenneker21@gmail.com>
2021-02-17 13:33:10 -0500

    added subject area for form

--- src/Contact.js
+++ src/Contact.js
@@ -13,6 +13,7 @@ const Contact = () => {
     < div >
       <h3>CONTACT</h3>
       {success && <p style={{ color: "green" }}>Thanks for your message! </p>}
+
       <div className="contact-wrap" style={{ margin: "0 20% 0 20%" }}>
         <form
           name="contact"
@@ -35,12 +36,21 @@ const Contact = () => {
           />
           <textarea
             required
+            label="subject"
+            name="subject"
+            placeholder=""
+          />
+          <textarea
+            required
             multiline
             label="message"
             name="message"
             placeholder="message"
           />
-          <button type="submit">Send</button>
+          <div class="row">
+            <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+            </button>
+          </div>
         </form>
       </div>
 
@@ -50,55 +60,9 @@ const Contact = () => {
 
 export default Contact
 
-// https://www.gatsbyjs.com/docs/building-a-contact-form#reach-skip-nav
 
-// GOOD: https://dev.to/deboragaleano/how-to-create-a-netlify-contact-form-in-react-5bma
-
-// < div >
-//     <h2> Contact</h2>
-//     <h5> lisavenneker21@gmail.com </h5>
 
 //     <img src="https://pics.freeicons.io/uploads/icons/png/4422549621553664904-512.png" alt="github logo" style={{ width: "8%", marginRight: "20%" }}></img>
 
 //     <img src="https://180dc.org/wp-content/uploads/2020/01/Linkedin-logo-1-550x550-300x300.png" alt="Linkedin logo" style={{ width: "10%" }}></img>
 
-//   </div >
-
-{/* <div className="row">
-        <form className="col s12"
-          name="contact"
-          method="POST"
-          data-netlify="true"
-        >
-          <div className="row">
-            <div className="input-field col s12">
-              <input placeholder="Name" id="first_name" type="text" className="validate"/>
-              <label for="first_name">Name</label>
-            </div>
-
-            <div className="row">
-              <div className="input-field col s12">
-                <input id="email" type="email" className="validate" />
-                <label for="email">Email</label>
-              </div>
-            </div>
-
-            <div className="row">
-              <div className="col s12">
-                <div className="row">
-                  <div className="input-field col s12">
-                    <textarea id="textarea1" className="materialize-textarea" type="text" ></textarea>
-                    <label for="textarea1"> Message</label>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="row">
-              <button class="btn waves-effect waves-light" type="submit" name="action">Submit
-              </button>
-            </div>
-
-          </div>
-        </form>
-      </div> */}
