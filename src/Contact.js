25db100d5f835e8346b6e2315b090e445dcd3efb
Lisa Wand <lisavenneker21@gmail.com>
2021-02-18 16:51:44 -0500

    padding adjust contact form for better ui

--- src/Contact.js
+++ src/Contact.js
@@ -9,9 +9,11 @@ const Contact = () => {
     }
   }, []);
 
+
+
   return (
     < div >
-      <div className="contact-border" style={{ margin: "0 15% 0 15%", border: "5px solid white", borderRadius: "24px", boxShadow: "rgb(238 238 238) 8px 8px 8px 8px", padding: "10px" }}>
+      <div className="contact-border" style={{ margin: "0 15% 0 15%", border: "5px solid white", borderRadius: "24px", boxShadow: "rgb(238 238 238) 8px 8px 8px 8px", padding: "15px" }}>
         <h3>CONTACT</h3>
         {success && <p style={{ color: "green" }}>Thanks for your message! </p>}
 
@@ -68,11 +70,11 @@ const Contact = () => {
       <div className="icon-wrap" style={{ marginBottom: "4%", marginTop: "3%" }} >
         <h3 style={{ marginBottom: "3%" }}>Connect</h3>
         <div style={{ display: 'inline', paddingRight: "5%" }}>
-          <img src="https://cdn.freelogovectors.net/wp-content/uploads/2016/12/github-logo-mark.png" alt="github logo" style={{ width: "6%", cursor: "pointer" }} onClick={() => window.open("https://github.com/LisaKVW", "_blank")} />
+          <img src="https://cdn.freelogovectors.net/wp-content/uploads/2016/12/github-logo-mark.png" alt="github logo" style={{ width: "8%", cursor: "pointer" }} onClick={() => window.open("https://github.com/LisaKVW", "_blank")} />
         </div>
 
         <div style={{ display: 'inline', paddingTop: "10px", paddingLeft: "3%" }}>
-          <img src="https://www.freepnglogos.com/uploads/linkedin-basic-round-social-logo-png-13.png" alt="Linkedin logo" style={{ width: "6%", cursor: "pointer" }} onClick={() => window.open("https://www.linkedin.com/in/lisa-venneker-wand/", "_blank")} ></img>
+          <img src="https://www.freepnglogos.com/uploads/linkedin-basic-round-social-logo-png-13.png" alt="Linkedin logo" style={{ width: "8%", cursor: "pointer" }} onClick={() => window.open("https://www.linkedin.com/in/lisa-venneker-wand/", "_blank")} ></img>
         </div>
       </div>
 
